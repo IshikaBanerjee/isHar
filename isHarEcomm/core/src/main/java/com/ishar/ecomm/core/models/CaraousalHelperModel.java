25cf53e5b635799a7c38071e98fef98d50f95733
IshikaBanerjee <ishikabanerjee9@gmail.com>
2018-09-07 00:41:40 +0530

    Editable Navigation Completed

--- isHarEcomm/core/src/main/java/com/ishar/ecomm/core/models/CaraousalHelperModel.java
+++ isHarEcomm/core/src/main/java/com/ishar/ecomm/core/models/CaraousalHelperModel.java
@@ -1,12 +1,14 @@
 package com.ishar.ecomm.core.models;
 import org.apache.sling.models.annotations.Model;
 import org.apache.sling.api.resource.Resource;
+import org.apache.sling.models.annotations.Optional;
+
 import javax.inject.Inject;
 
 
 @Model(adaptables= Resource.class)
 public class CaraousalHelperModel {
-    @Inject
+    @Inject @Optional
     private String[] images;
     public String[] getImages()
     {
--- isHarEcomm/core/src/main/java/com/ishar/ecomm/core/models/NavigationHelperModel.java
+++ isHarEcomm/core/src/main/java/com/ishar/ecomm/core/models/NavigationHelperModel.java
@@ -2,32 +2,67 @@ package com.ishar.ecomm.core.models;
 
 import com.day.cq.wcm.api.Page;
 import com.day.cq.wcm.api.PageManager;
+//import com.ishar.ecomm.core.servlets.PathToPageService;
+import org.apache.felix.scr.annotations.Reference;
 import org.apache.sling.api.resource.Resource;
 import org.apache.sling.api.resource.ResourceResolver;
+import org.apache.sling.api.resource.ResourceResolverFactory;
 import org.apache.sling.models.annotations.Model;
+import org.apache.sling.models.annotations.Optional;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.inject.Inject;
+import javax.jcr.Node;
+import javax.jcr.Session;
 import java.util.ArrayList;
+
 
 @Model(adaptables= Resource.class)
 public class NavigationHelperModel {
-    @Inject
+    private static final Logger log = LoggerFactory.getLogger(NavigationHelperModel.class);
+    @Inject @Optional
     private String[] navbuttons;
-    @Inject
-    ResourceResolver resourceResolver;
-    @Inject
-    private ArrayList<Page> pages;
+    @Inject @Optional
+    private Page[] pages;
 
-    //ResourceResolver resourceResolver=resourceResolverFactory.getAdministrativeResourceResolver();
-    PageManager pageManager=resourceResolver.adaptTo(PageManager.class);
+    @Inject
+    private ResourceResolverFactory resourceResolverFactory;
+//    @Inject
+//    PathToPageService pathToPageService;
 
-    public ArrayList<Page> getPages()
+    ResourceResolver resourceResolver = null;
+    PageManager pageManager;
+    private Session session;
+
+    public Page[] getPages()
     {
-        for(String navbutton:navbuttons)
-        {
-            Page navPage=pageManager.getPage(navbutton);
-            pages.add(navPage);
+//        Map<String, Object> param = new HashMap<String, Object>();
+//        param.put(ResourceResolverFactory.SUBSERVICE, "datawrite");
+        int count=navbuttons.length;
+        ArrayList pagearray=new ArrayList<Page>();
+        try {
+            resourceResolver = resourceResolverFactory.getAdministrativeResourceResolver(null);
+            session = resourceResolver.adaptTo(Session.class);
+            Node root = session.getRootNode();
+            for (String navbutton:navbuttons)
+            {
+//                Node content = root.getNode(navbutton);
+//                Page page=content   //pageManager.getPage(content.getPath());
+                Page page=resourceResolver.getResource(navbutton).adaptTo(Page.class);
+                String name=page.getTitle();
+                log.error("Page is:"+name);
+                System.out.print(page.getTitle());
+                pagearray.add(page);
+
+            }
+
+            pages= (Page[]) pagearray.toArray(new Page[pagearray.size()]);
+
+        } catch (Exception e1) {
+            e1.printStackTrace();
         }
+
         return pages;
     }
     public String[] getNavbuttons()
